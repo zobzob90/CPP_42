f32f72083e0bcf0abc28d387632ff316844f1dce
Eric Trignau <ertrigna@f6r7s12.clusters.42paris.fr>
2025-08-19 15:02:19 +0200

    ex01 good

--- cpp_02/ex01/main.cpp
+++ cpp_02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ertrigna <ertrigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 15:13:41 by ertrigna          #+#    #+#             */
-/*   Updated: 2025/08/13 16:50:17 by ertrigna         ###   ########.fr       */
+/*   Updated: 2025/08/19 11:54:09 by ertrigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,22 @@
 
 int	main(void)
 {
-	Fixed		a;
-	Fixed const	b(Fixed( 5.05f ) * Fixed( 2 ));
-
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-
-	std::cout << b << std::endl;
+	Fixed	a;
+	Fixed	const b( 10 );
+	Fixed	const c( 42.42f);
+	Fixed const d ( b );
 	
-	std::cout << Fixed::max( a, b ) << std::endl;
+	a = Fixed( 1234.4321f );
+	
+	std::cout << "a is " << a <<  std::endl;
+	std::cout << "b is " << b <<  std::endl;
+	std::cout << "c is " << c <<  std::endl;
+	std::cout << "d is " << d <<  std::endl;
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	
 	return (0);
 }
--- cpp_02/ex02/main.cpp
+++ cpp_02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ertrigna <ertrigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 15:13:41 by ertrigna          #+#    #+#             */
-/*   Updated: 2025/08/13 15:18:28 by ertrigna         ###   ########.fr       */
+/*   Updated: 2025/08/19 12:16:52 by ertrigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,17 @@
 
 int	main(void)
 {
-	Fixed		a;
-	Fixed const	b( 10 );
-	Fixed const	c( 42.42f );
-	Fixed const	d( b );
+	Fixed	a;
+	Fixed	const	b ( Fixed( 5.05f ) * Fixed ( 2 ));
 
-	a = Fixed( 1234.4321f );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
 
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-	
+	std::cout << b << std::endl;
+	std::cout << Fixed::min( a, b ) << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
 	return (0);
 }
